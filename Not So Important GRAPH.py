89037d124447f4b8530cffe76abb42c5ab4dc307
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2021-09-01 23:09:23 +0600

    Update Not So Important GRAPH.py

--- Not So Important GRAPH.py
+++ Not So Important GRAPH.py
@@ -120,4 +120,37 @@ dfs1(x,12)
 #bfs(x,1,12)
 
 
+############################################################################
+def djikstra(graph,start,goal):
+    short = {}
+    predecessor = {}
+    unseen = graph
+    infinity = 999999
+    path = []
+    for i in unseen:
+        short[i] = infinity
+    short[start] = 0
 
+    while len(unseen)>0:
+        minNode = None
+        for node in unseen:
+            if minNode==None: minNode = node
+            elif short[node] <short [minNode]: minNode = node
+        for childNode, weight in graph [minNode].items():
+            if weight + short[minNode] < short[childNode]:
+                short [childNode] = weight + short[minNode]
+                predecessor[childNode] = minNode
+        unseen.pop(minNode)
+    print(short)        
+
+
+graph = {
+    'kafrul' :{'mirpur-14':5,'cantonment':1},
+    'mirpur-14':{'mirpur-11':3,'kafrul':5},
+    'cantonment':{'mirpur-14':9,'farmgate':8,'stadium':14},
+    'farmgate':{'cantonment':8},
+    'mirpur-11':{'stadium':2},
+    'stadium':{'mirpur-11':2}
+}
+
+djikstra(graph,'kafrul','stadium')
