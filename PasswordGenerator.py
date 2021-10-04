35f5c2540933306dca7315e7b8b699bb96345f00
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2021-10-05 03:10:38 +0600

    Generates 16 digit passwords

---
+++ PasswordGenerator.py
@@ -0,0 +1,21 @@
+import random
+class mongodb:
+    def __init__(self,text1,text2):
+        self.text1=text1
+        self.text2=text2
+    def passgenerator(self):
+        a = ''
+        alpha, beta, number = [],[],[]
+        for i in range(65,91): alpha.append(chr(i));
+        for i in range(97,123): beta.append(chr(i));
+        for i in range(0,10): number.append(str(i));
+        alll = [alpha,beta,number]
+        for i in range(0,16):
+            a += random.choice(random.choice(alll))
+        return a
+    
+    def createAccount(self,name,id_number):
+        pass
+
+
+print(mongodb('test','test').passgenerator())
