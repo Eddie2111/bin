ec1d12e2513df9f5f9063ffca6f1d3f5e20b8591
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-10-02 03:00:30 +0600

    Create PythonRedis.py

---
+++ PythonRedis.py
@@ -0,0 +1,15 @@
+#### typescripting is extremely important
+main = dict(
+    {"key1":"value1"}
+    )
+    
+def consume(key,value):
+    main[key]=value;
+def find(key):
+    return main[key]
+    
+    
+consume("segs1","211")
+
+print(find("segs1"))
+    
